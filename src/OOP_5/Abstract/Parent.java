feb63df7bfe36e36714f86ec32acab412ae5f8b3
JatinKishore <jatinkm02@gmail.com>
2023-11-03 21:58:09 +0530

    OOP_4

---
+++ src/OOP_5/Abstract/Parent.java
@@ -0,0 +1,24 @@
+package OOP_5.Abstract;
+/*All the abstract methods needs to be overriden the child class*/
+public abstract class Parent  {// a class with atleast one abstract method needs to be written as abstract class
+    static int age;
+    public  Parent(int a){
+        age = a;
+    }
+    //abstract Parent(); its an error abstract constructors are not allowes
+
+    //abstract static method cannot be built.. because abstract methods needs to be overriden whereas static methods cannot be overriden
+
+    static void hello() {//but we can create ordinary static methods
+    }
+
+    void normal(){
+        System.out.println("Dont be ordinary be extra- ordinary");
+    }
+
+
+    abstract void career();
+    abstract void partner();
+
+
+}
