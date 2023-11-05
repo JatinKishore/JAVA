0d9c919b50c0470eb738563595d1a369bbf95482
JatinKishore <jatinkm02@gmail.com>
2023-11-05 08:36:59 +0530

    OOP_6

---
+++ src/OOP_6/MyException.java
@@ -0,0 +1,8 @@
+package OOP_6;
+
+public class MyException extends  Exception {
+    public  MyException(String message){
+        super(message);//This will call the main exception constructor.. since we are extending the Exception class
+    }
+
+}
